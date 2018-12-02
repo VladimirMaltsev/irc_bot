f12f6fc8dcb79389a54e711f780045342078b2be
VladimirMaltsev <vov.maltsev@gmail.com>
2018-12-02 17:06:34 +0300

    bot testing

---
+++ IrcBot/BotManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+    class Program
+    {
+        static void Main(string[] args) { 
+
+         IrcBot ircBot;
+
+        
+             ircBot = new IrcBot(
+                 server: "irc.freenode.net",
+                 port: 6667,
+                 user: "USER BOTVOTr 0 * :BOTVOTr",
+                 nick: "GadalkaLola",
+                 channel: "##net1337",
+                 maxRetries: 10
+                 );
+
+        new Thread(() => ircBot.ReadFromChannel()).Start();
+        new Thread(() => ircBot.WriteToChannel()).Start();
+        
+    }
+}
+
